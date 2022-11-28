7be6fcbd5dee748781758c44c52229d0d9519c80
Batuhan Celik <batuhancelik.boun@gmail.com>
2022-11-28 19:15:12 +0300

    give faucets implementation

--- MyGov.sol
+++ MyGov.sol
@@ -4,13 +4,15 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract GOVToken is ERC20 {
     address owner;
-
+    mapping(address => bool) givenFaucets;
     constructor(uint256 initialSupply) ERC20("MyGov", "MGV") {
         owner = msg.sender;
         _mint(owner, initialSupply * (10**18));
     }
 
     function Faucet() public {
+        require(!(givenFaucets[msg.sender]));
+        givenFaucets[msg.sender] = true;
         super._transfer(owner, msg.sender, 10**18);
     }
 }
