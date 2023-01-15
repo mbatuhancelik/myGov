71416dae7846efcf0abb69d7f2a204a8acbaabf6
Batuhan Celik <batuhancelik.boun@gmail.com>
2023-01-15 21:19:12 +0300

    change contract address

--- front/src/Contracts/contracts.js
+++ front/src/Contracts/contracts.js
@@ -3,7 +3,7 @@ import { utils } from 'ethers'
 import { Contract } from '@ethersproject/contracts'
 import { Signer } from 'ethers/lib/ethers.js';
 
-export const contractAddress = "0x7c573Da5C1ac70AB55a64a693D9dF1FCFDe3Ae7a";
+export const contractAddress = "0x1f51Ce46a41792BAA4274524c69a02BD2950f6BD";
 export const Abi = ABI;
 
 export const mygovInterface = new utils.Interface(Abi)
