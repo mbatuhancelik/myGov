7a88835bfc5b2e718daae4413828c707ed453981
onurkmr <konur2727@gmail.com>
2023-01-15 22:21:26 +0300

    added undo delegate vote.

--- front/src/myGov/delegateVote.js
+++ front/src/myGov/delegateVote.js
@@ -9,7 +9,8 @@ import { Contract } from '@ethersproject/contracts'
 
 
 export default  function DelegateVote(){
-    const { state, send } = useContractFunction(contract, 'delegateVoteTo', {});
+    const { state: state1, send: send1 } = useContractFunction(contract, 'delegateVoteTo', {});
+    const { state: state2, send: send2 } = useContractFunction(contract, 'cancelDelegation', {});
     const [address, setAddress] = useState("")
     const [amount, setAmount] = useState("")
 
@@ -20,9 +21,14 @@ export default  function DelegateVote(){
         </Typography>
         <TextField type="string" style={{ marginBottom: 20 }} fullWidth label="Member Address" value={address} onChange={e => setAddress(e.target.value)} />
         <TextField type="int" style={{ marginBottom: 20 }} fullWidth label="Project ID" value={amount} onChange={e => setAmount(e.target.value)} />
-        <Button variant="contained" fullWidth onClick={() => { void send(address, amount)}}>Delegate Vote</Button>
+        <Button variant="contained" fullWidth onClick={() => { void send1(address, amount)}}>Delegate Vote</Button>
         <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
-                {(state.transaction !== undefined && state.status !== 'None') ? "Successfully delegated vote to!" + setAddress: (state.status === "Exception" ? `Exception Details: ${state.errorMessage}` : "")}
+                {(state1.transaction !== undefined && state1.status !== 'None') ? "Successfully delegated vote to " + address: (state1.status === "Exception" ? `Exception Details: ${state1.errorMessage}` : "")}
+        </Typography>
+
+        <Button variant="contained" fullWidth onClick={() => { void send2()}}>Undo All Delegations</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
+                {(state2.transaction !== undefined && state2.status !== 'None') ? "Successfully undo all delegations!": (state2.status === "Exception" ? `Exception Details: ${state2.errorMessage}` : "")}
         </Typography>
         </div>
 
