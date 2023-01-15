fd19b4e740f1577e435193c94dd3b1af9d3142d7
onurkmr <konur2727@gmail.com>
2023-01-15 13:54:45 +0300

    First commit

---
+++ front/src/myGov/submitProjectProposal.js
@@ -0,0 +1,29 @@
+import { useContractFunction} from "@usedapp/core"
+import Button from "@mui/material/Button";
+import { contract} from "../Contracts/contracts.js";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import {useState} from "react";
+import { utils } from 'ethers'
+import { Contract } from '@ethersproject/contracts'
+
+
+export default  function GetBalance(){
+    const { state, send } = useContractFunction(contract, 'submitProjectProposal', {});
+    const [address, setAddress] = useState("")
+
+    return (
+        <div>
+        <Typography variant="h2" component="div" gutterBottom align={"center"}>
+                See balance of the address
+        </Typography>
+        <TextField type="string" style={{ marginBottom: 20 }} fullWidth label="Address" value={address} onChange={e => setAddress(e.target.value)} />
+        <Button variant="contained" fullWidth onClick={() => { void send(address)}}>Get Balance</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
+                {(state.transaction !== undefined && state.status !== 'None') ? "User balance is " + parseInt(state.transaction._hex): (state.status === "Exception" ? `Exception Details: ${state.errorMessage}` : "")}
+        </Typography>
+        </div>
+
+
+    );
+}
