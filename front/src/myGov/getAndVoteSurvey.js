0913f28c52e3a3b5fd325bd577cf1e41358ea800
onurkmr <konur2727@gmail.com>
2023-01-15 23:42:50 +0300

    added get and vote survey and minor design changes.

--- front/src/myGov/getAndVoteSurvey.js
+++ front/src/myGov/getAndVoteSurvey.js
@@ -7,12 +7,18 @@ import {useState} from "react";
 import { utils } from 'ethers'
 import { Contract } from '@ethersproject/contracts'
 
-
+function NewlineText(props) {
+    const text = props.text;
+    const newText = text.split('\n').map(str => <p>{str}</p>);
+    
+    return newText;
+  }
+  
 export default  function GetBalance(){
     const { state: stateSurveyInfo, send: sendSurveyInfo } = useContractFunction(contract, 'getSurveyInfo', {});
     const { state: stateSurveyOwner, send: sendSurveyOwner } = useContractFunction(contract, 'getSurveyOwner', {});
     const { state: stateSurveyResults, send: sendSurveyResults } = useContractFunction(contract, 'getSurveyResults', {});
-    const { state: stateVoting, send: sendVotering } = useContractFunction(contract, 'takeSurvey', {});
+    const { state: stateVoting, send: sendVoting } = useContractFunction(contract, 'takeSurvey', {});
 
     const [surveyID, setSurveyID] = useState("")
     const [surveyChoices, setSurveyChoices] = useState("")
@@ -23,19 +29,35 @@ export default  function GetBalance(){
                 Survey related actions
         </Typography>
         <TextField type="int" style={{ marginBottom: 20 }} fullWidth label="Survey ID" value={surveyID} onChange={e => setSurveyID(e.target.value)} />
-        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyOwner(surveyID); console.log(stateSurveyOwner.transaction)}}>Get Survey Owner</Button>
-        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
+        <TextField type="int[]" style={{ marginBottom: 20 }} fullWidth label="Survey Choices" value={surveyChoices} onChange={e => setSurveyChoices(e.target.value)} />
+        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyOwner(surveyID)}}>Get Survey Owner</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='10px' marginBottom='30px'>
                 {(stateSurveyOwner.transaction !== undefined && stateSurveyOwner.status !== 'None') ? "Owner of the survey " + surveyID + " is " + stateSurveyOwner.transaction : (stateSurveyOwner.status === "Exception" ? `Exception Details: ${stateSurveyOwner.errorMessage}` : "")}
         </Typography>
 
-        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyInfo(surveyID); console.log(stateSurveyInfo.transaction)}}>Get Survey Details</Button>
-        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
-                {(stateSurveyInfo.transaction !== undefined && stateSurveyInfo.status !== 'None') ? "Info of the survey " + surveyID + " is " + stateSurveyInfo.transaction : (stateSurveyInfo.status === "Exception" ? `Exception Details: ${stateSurveyInfo.errorMessage}` : "")}
+        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyInfo(surveyID)}}>Get Survey Details</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='10px' marginBottom='30px'>
+                {(stateSurveyInfo.transaction !== undefined && stateSurveyInfo.status !== 'None') ? 
+                <NewlineText text={(
+                'Details of the survey:\n Survey deadline: ' + stateSurveyInfo.transaction.surveydeadline + '\nNumber of choices: ' + stateSurveyInfo.transaction.numchoices 
+                + '\nMaximum number of selectable choices: ' + stateSurveyInfo.transaction.atmostchoice)} />
+                : (stateSurveyInfo.status === "Exception" ? `Exception Details: ${stateSurveyInfo.errorMessage}` : "")}
         </Typography>
 
-        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyResults(surveyID); console.log(stateSurveyInfo.transaction)}}>Get Survey Results</Button>
-        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='40px'>
-                {(stateSurveyResults.transaction !== undefined && stateSurveyResults.status !== 'None') ? "Info of the survey " + surveyID + " is " + stateSurveyResults.transaction : (stateSurveyResults.status === "Exception" ? `Exception Details: ${stateSurveyResults.errorMessage}` : "")}
+        <Button variant="contained" disabled={surveyID.length == 0} fullWidth onClick={() => { void sendSurveyResults(surveyID); console.log(stateSurveyResults.transaction)}}>Get Survey Results</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='10px' marginBottom='30px'>
+                {(stateSurveyResults.transaction !== undefined && stateSurveyResults.status !== 'None') ? 
+                <NewlineText text={(
+                'Survey results:\n Number of voters: ' + stateSurveyResults.transaction.numtaken + '\nResults: ' + stateSurveyResults.transaction.results)} />
+                
+                : (stateSurveyResults.status === "Exception" ? `Exception Details: ${stateSurveyResults.errorMessage}` : "")}
+        </Typography>
+
+        <Button variant="contained" disabled={surveyID.length == 0 || surveyChoices.length == 0} fullWidth onClick={() => { void sendVoting(surveyID, surveyChoices); console.log(surveyChoices)}}>Vote the survey</Button>
+        <Typography variant="p1" component="div" gutterBottom align={"center"} marginTop='10px' marginBottom='30px'>
+                {(stateVoting.transaction !== undefined && stateVoting.status !== 'None') ? 
+                "You voted the survey successfully!"
+                : (stateSurveyResults.status === "Exception" ? `Exception Details: ${stateSurveyResults.errorMessage}` : "")}
         </Typography>
         </div>
 
